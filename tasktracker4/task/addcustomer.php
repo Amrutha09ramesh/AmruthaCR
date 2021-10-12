0bc0630c2415eefd7bb62ef414b873a06060cbcd
Amrutha09ramesh <90747433+Amrutha09ramesh@users.noreply.github.com>
2021-10-12 12:06:21 +0530

    Add files via upload

---
+++ tasktracker4/task/addcustomer.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding Customer</title>
+    <style>
+        body
+        {
+            margin-top:0px;
+            padding: 0px;
+            background-color: #5F4DB6;
+            color: white;
+            font-family: 'Times New Roman', Times, serif;
+        }
+        table, td, th
+        {
+            border: 1px solid black;
+            padding:5px;
+        }
+        .container
+        {
+            margin-top: 250px;
+        }
+        a
+        {
+            color: white;
+            text-decoration: none;
+            padding: 10px;
+            margin: 20px;
+        }
+        a,button
+        {
+            background-color: blue;
+            color: white;
+            padding: 10px;
+            margin-right: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+        } 
+        th
+        {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <header>
+            <a href="index.php" button type="button">Home</a>
+        </header>
+        </div>
+        <div class="container">
+        <center>
+        <table>
+            <tr>
+                <th > Customer </th>
+                <th> Phone No </th>
+                <th> Mobile </th>
+                <th> Add Customer </th>
+            </tr>
+            <tr>
+                <td> ShashiKanth  </td><!-- Here u can fill the name-->
+                <td> N/A </td>
+                <td> 9999999007 </td>
+                <td> <input type="text"  placeholder="Add Customer"> </td>
+            </tr>
+            <tr>
+                <td> Amrutha </td>
+                <td> N/A </td>
+                <td> 9999999008 </td>
+                <td> <input type="text"  placeholder="Add Customer"> </td>
+            </tr>
+            <tr>
+                <td> ShashiKanth  </td>
+                <td> N/A </td>
+                <td> 9999999007 </td>
+                <td> <input type="text" placeholder="Add Customer"> </td>
+            </tr>
+            <tr>
+                <td> ShashiKanth  </td>
+                <td> N/A </td>
+                <td> 9999999007 </td>
+                <td> <input type=textn"  placeholder="Add Customer"> </td>
+            </tr>
+            <tr>
+                <td> ShashiKanth  </td>
+                <td> N/A </td>
+                <td> 9999999007 </td>
+                <td> <input type="text"  placeholder="Add Customer"> </td>
+            </tr>
+        </table>
+    </center>
+</body>
+</html>
---
+++ tasktracker4/task/addtask.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Task</title>
+    <style>
+        body
+        {
+            margin-top:0px;
+            padding: 0px;
+            background-color: #5F4DB6;
+            color: white;
+            font-family: 'Times New Roman', Times, serif;
+        }
+        table, td, th
+        {
+            border: 1px solid black;
+            padding:5px;
+        }
+        .container
+        {
+            margin-top: 250px;
+        }
+        a
+        {
+            color: white;
+            text-decoration: none;
+            padding: 10px;
+            margin: 20px;
+        }
+        a,button
+        {
+            background-color: blue;
+            color: white;
+            padding: 10px;
+            margin-right: 10px;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+    <header>
+        <a href="index.php" button type="button">Home</a>
+    </header>
+    </div>
+    <div class="container">
+    <center>
+    <table>
+        <tr>
+            <th> Name </th>
+            <th> Visit Time </th>
+            <th> Status </th>
+            <th> Add Task </th>
+        </tr>
+        <tr>
+            <td> Example </td>
+            <td> 9:30 AM </td>
+            <td> Pending </td>
+            <td> <input type="text" placeholder="Enter the user Task "> </td>
+        </tr>
+        <tr>
+            <td> Example </td>
+            <td> 9:30 AM </td>
+            <td> Pending </td>
+            <td> <input type="text" placeholder="Enter the user Task "> </td>
+        </tr>
+        <tr>
+            <td> Example </td>
+            <td> 9:30 AM </td>
+            <td> Pending </td>
+            <td> <input type="text" placeholder="Enter the user Task "> </td>
+        </tr>
+        <tr>
+            <td> Example </td>
+            <td> 9:30 AM </td>
+            <td> Pending </td>
+            <td> <input type="text" placeholder="Enter the user Task "> </td>
+        </tr>
+        <tr>
+            <td> Example </td>
+            <td> 9:30 AM </td>
+            <td> Pending </td>
+            <td> <input type="text" placeholder="Enter the user Task "> </td>
+        </tr>
+
+    </table>
+</center>
+</div>
+</body>
+</html>
